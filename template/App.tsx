07c6a43f95ff8b0cd2a13570928da041ba3f4d3a
KatsuyaAkasaka <akasakatora1208@gmail.com>
2021-11-03 20:53:00 +0900

    fix format target file

--- template/App.tsx
+++ template/App.tsx
@@ -30,7 +30,11 @@ const App: React.FC = () => {
   const [isLoadingComplete, setLoadingComplete] = useState(false)
   const _loadResourcesAsync = async () => {
     await Promise.all([
-      Asset.loadAsync([require('./assets/splash.png'), require('./assets/icon.png'), require('./assets/favicon.png')]),
+      Asset.loadAsync([
+        require('./assets/splash.png'),
+        require('./assets/icon.png'),
+        require('./assets/favicon.png'),
+      ]),
       Font.loadAsync({
         ...FontAwesome.font,
       }),
@@ -40,12 +44,23 @@ const App: React.FC = () => {
   const _handleLoadingError = (error: Error) => {}
   const _handleFinishLoading = () => setLoadingComplete(true)
   if (!isLoadingComplete) {
-    return <AppLoading startAsync={_loadResourcesAsync} onError={_handleLoadingError} onFinish={_handleFinishLoading} />
+    return (
+      <AppLoading
+        startAsync={_loadResourcesAsync}
+        onError={_handleLoadingError}
+        onFinish={_handleFinishLoading}
+      />
+    )
   } else {
     return (
       <SafeAreaProvider>
         <NativeBaseProvider theme={theme}>
-          <Center _dark={{ bg: 'blueGray.900' }} _light={{ bg: 'blueGray.50' }} px={4} flex={1}>
+          <Center
+            _dark={{ bg: 'blueGray.900' }}
+            _light={{ bg: 'blueGray.50' }}
+            px={4}
+            flex={1}
+          >
             <VStack space={5} alignItems='center'>
               <Heading size='lg'>Welcome to NativeBase</Heading>
               <HStack space={2} alignItems='center'>
@@ -75,7 +90,9 @@ function ToggleDarkMode() {
       <Switch
         isChecked={colorMode === 'light' ? true : false}
         onToggle={toggleColorMode}
-        aria-label={colorMode === 'light' ? 'switch to dark mode' : 'switch to light mode'}
+        aria-label={
+          colorMode === 'light' ? 'switch to dark mode' : 'switch to light mode'
+        }
       />
       <Text>Light</Text>
     </HStack>
