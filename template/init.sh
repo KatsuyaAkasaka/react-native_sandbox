8db5f75c95f381dc109fe293725a154977ec1370
KatsuyaAkasaka <akasakatora1208@gmail.com>
2021-11-07 18:38:51 +0900

    add option

--- template/init.sh
+++ template/init.sh
@@ -2,5 +2,5 @@
 npm i -g expo-cli
 expo init $1 --yarn --template="expo-template-blank-typescript"  # expo init nyannyan --yarn --template="expo-template-blank-typescript"
 cd $1
-curl https://raw.githubusercontent.com/KatsuyaAkasaka/react-native_sandbox/master/template/Makefile > Makefile
+curl -sSfL https://raw.githubusercontent.com/KatsuyaAkasaka/react-native_sandbox/master/template/Makefile > Makefile
 make init
